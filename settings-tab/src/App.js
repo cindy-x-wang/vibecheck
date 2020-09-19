4b2ad35bbfcc46d0d993d639bd2dffd3d71e90b1
Helen Lu <helen_lu@mit.edu>
2020-09-19 15:28:19 -0700

    implemented more front end web app structure

--- settings-tab/src/App.js
+++ settings-tab/src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Group from './Group.js';
+
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      groupMembers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15],
+      groupName: 'heckMIT',
+      groups: {
+        'heckMIT':  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15],
+        'herMIT': [1,2,3,4,5,6],
+        'kerMIT': [2,3,4],
+        'turNIP': [2, 4, 6, 10]
+      }
+    }
 
   }
 
 
   render() {
+    let first = true;
+    let groups = [];
+    console.log(Object.entries(this.state.groups));
+    for(const [groupName, groupMembers] of Object.entries(this.state.groups)) {
+      groups.push(<Group groupMembers={groupMembers} groupName={groupName} first={first}/>);
+      first = false;  
+    }
+
     return (
       <>
         <div className='App-container'> 
@@ -18,8 +38,12 @@ class App extends React.Component {
             <div className='App-title'><em>~vibe check~</em></div>
 
             <div className='App-fieldContainer'> 
-              <div className='App-fields'> screen name: </div>
-              <input className='App-name'/>
+              <div className='App-fields'> screen name </div>
+              <div className='App-nameContainer'>   
+                <input className='App-name'/>
+                <div className='App-nameSubmit'> change </div>
+              </div>
+              
             </div>
 
             <div className='App-fieldContainer'> 
@@ -30,9 +54,9 @@ class App extends React.Component {
               </div>
             </div>
 
-            <div style={{border: '2px solid black', height: 30, marginBottom: 32}}> TEMPORARY GROUP MEMBER PLACEMENT </div>
+            <Group groupMembers={this.state.groupMembers} groupName={this.state.groupName} first={true}/>
 
-            <div className='App-fieldContainer'> 
+            <div className='App-fieldContainer' style={{marginTop: 32}}> 
               <div className='App-fields'> create new group </div>
               <div className='App-inputContainer'>
                 <input />
@@ -41,10 +65,10 @@ class App extends React.Component {
             </div>
 
             <div className='App-displayGroups'> 
-              <div className='App-fields'> your current groups </div>
+              <div className='App-fields App-group'> your current groups </div>
+              {groups}
             </div>
 
-            <div style={{border: '2px solid black', height: 30, marginBottom: 32}}> TEMPORARY GROUPS PLACEMENT </div>
 
           </div>
         </div>
