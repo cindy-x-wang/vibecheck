aaca8a917f77d62d1a226a96fba602f85d25be84
Helen Lu <helen_lu@mit.edu>
2020-09-19 13:42:48 -0700

    implemented frontend layout for web app

--- settings-tab/src/App.js
+++ settings-tab/src/App.js
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  render() {
+    return (
+      <>
+        <div className='App-container'> 
+          <div className='App-vibeBox'> 
+            <div className='App-title'><em>~vibe check~</em></div>
+
+            <div className='App-fieldContainer'> 
+              <div className='App-fields'> screen name: </div>
+              <input className='App-name'/>
+            </div>
+
+            <div className='App-fieldContainer'> 
+              <div className='App-fields'> join new group </div>
+              <div className='App-inputContainer'>
+                <input />
+                <input />               
+              </div>
+            </div>
+
+            <div style={{border: '2px solid black', height: 30, marginBottom: 32}}> TEMPORARY GROUP MEMBER PLACEMENT </div>
+
+            <div className='App-fieldContainer'> 
+              <div className='App-fields'> create new group </div>
+              <div className='App-inputContainer'>
+                <input />
+                <input />               
+              </div>
+            </div>
+
+            <div className='App-displayGroups'> 
+              <div className='App-fields'> your current groups </div>
+            </div>
+
+            <div style={{border: '2px solid black', height: 30, marginBottom: 32}}> TEMPORARY GROUPS PLACEMENT </div>
+
+          </div>
+        </div>
+
+      </>
+    );
+  }
+}  
 
 export default App;
+
