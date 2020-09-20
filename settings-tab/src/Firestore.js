db479fe8a81de5f297232052bef19ed2ff0b211a
Helen Lu <helen_lu@mit.edu>
2020-09-19 23:08:47 -0700

    implemented client side group creation + join

---
+++ settings-tab/src/Firestore.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+   apiKey: 'AIzaSyAY8N97fSGhk89Ik-Hx4S1biySFxu7Qq9k',
+   authDomain: 'https://vibecheck-hack.firebaseapp.com',
+   databaseURL: 'https://vibecheck-hack.firebaseio.com',
+   projectId: 'vibecheck-hack',
+};
+
+export default firebase.initializeApp(firebaseConfig);
+
+
